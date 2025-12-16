5518caaedd865cb47df169ae132d6191341b22b7
shaistha-0527 <shaistha.cs24@bmsce.ac.in>
2025-12-16 23:10:27 +0530

    Update InheritanceExceptionDemo.java

--- lab7/InheritanceExceptionDemo.java
+++ lab7/InheritanceExceptionDemo.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class WrongAge extends Exception {
     public WrongAge(String message) {
         super(message);
@@ -21,21 +23,36 @@ class Son extends Father {
     Son(int fatherAge, int sonAge) throws WrongAge {
         super(fatherAge);
 
-        if (sonAge < 0 || sonAge >= fatherAge) {
-            throw new WrongAge("Invalid son's age");
+        if (sonAge < 0) {
+            throw new WrongAge("Son's age cannot be negative");
         }
+
+        if (sonAge >= fatherAge) {
+            throw new WrongAge("Son's age must be less than father's age");
+        }
+
         this.sonAge = sonAge;
     }
 }
 
-public class InheritanceExceptionDemo {
+public class sonfather {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter father's age: ");
+        int fAge = sc.nextInt();
+
+        System.out.print("Enter son's age: ");
+        int sAge = sc.nextInt();
+
         try {
-            Son s = new Son(45, 20);
-            System.out.println("Father Age: " + s.fatherAge);
-            System.out.println("Son Age: " + s.sonAge);
+            Son obj = new Son(fAge, sAge);
+            System.out.println("Son's age is: " + obj.sonAge);
+            System.out.println("Father's age is: " + obj.fatherAge);
         } catch (WrongAge e) {
-            System.out.println(e.getMessage());
+            System.out.println("Error: " + e.getMessage());
         }
+
+        sc.close();
     }
 }
