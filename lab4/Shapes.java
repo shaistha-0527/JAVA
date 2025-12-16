94c3df4187b4ba0f8933d5fd3f16ea5d8992e095
shaistha-0527 <shaistha.cs24@bmsce.ac.in>
2025-12-16 21:20:09 +0530

    Create Shapes.java

---
+++ lab4/Shapes.java
@@ -0,0 +1,50 @@
+abstract class Shape {
+    int a;
+    int b;
+
+    abstract void printArea();
+}
+
+class Rectangle extends Shape {
+    Rectangle(int length, int breadth) {
+        a = length;
+        b = breadth;
+    }
+
+    void printArea() {
+        System.out.println("Area of Rectangle: " + (a * b));
+    }
+}
+
+class Triangle extends Shape {
+    Triangle(int base, int height) {
+        a = base;
+        b = height;
+    }
+
+    void printArea() {
+        System.out.println("Area of Triangle: " + (0.5 * a * b));
+    }
+}
+
+class Circle extends Shape {
+    Circle(int radius) {
+        a = radius;
+    }
+
+    void printArea() {
+        System.out.println("Area of Circle: " + (3.14 * a * a));
+    }
+}
+
+public class Shapes {
+    public static void main(String[] args) {
+        Shape r = new Rectangle(10, 5);
+        Shape t = new Triangle(8, 6);
+        Shape c = new Circle(7);
+
+        r.printArea();
+        t.printArea();
+        c.printArea();
+    }
+}
